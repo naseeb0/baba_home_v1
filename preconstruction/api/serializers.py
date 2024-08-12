4ea23ec2964402cdf0e0ebbde1eea972723299f0
naseeb0 <nasheeb.dangi@gmail.com>
2024-08-12 11:14:22 +0545

    POUSH

--- preconstruction/api/serializers.py
+++ preconstruction/api/serializers.py
@@ -7,7 +7,9 @@ from preconstruction.models import PreConstruction, Developer,City,PreConstructi
 class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
-        fields = "__all__"
+        fields = ['id', 'name', 'city_lat', 'city_long', 'city_details']
+        extra_kwargs = {'city_lat':{'required':False}, 
+                        'city_long':{'required':False}}
 
 class PreConstructionImageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,25 +20,69 @@ class DeveloperSerializer(serializers.ModelSerializer):
     # preconstructions = PreConstructionSerializer(many=True, read_only=True)
     class Meta:
         model = Developer
-        fields = "__all__"
+        fields = ['id', 'name', 'website', 'details','slug']
+        extra_kwargs = {'slug':{'required':False}}
 
+
+
+# class PreConstructionSerializer(serializers.ModelSerializer):
+#     images = PreConstructionImageSerializer(many=True, required=False)
+#     developer = serializers.PrimaryKeyRelatedField(queryset=Developer.objects.all(), required=True)
+#     city = serializers.PrimaryKeyRelatedField(queryset=City.objects.all(), required=True)
+
+#     class Meta:
+#         model = PreConstruction
+#         fields = "__all__"
+
+#     def create(self, validated_data):
+#         images_data = validated_data.pop('images', [])
+#         developer = validated_data.pop('developer', None)
+#         city = validated_data.pop('city', None)
+
+#         if not developer or not city:
+#             raise serializers.ValidationError("Developer and City are required.")
+
+#         preconstruction = PreConstruction.objects.create(developer=developer, city=city, **validated_data)
+
+#         for image_data in images_data:
+#             PreConstructionImage.objects.create(preconstructionImage=preconstruction, **image_data)
+
+#         return preconstruction
 
 class PreConstructionSerializer(serializers.ModelSerializer):
     images = PreConstructionImageSerializer(many=True, required=False)
-    developer = serializers.PrimaryKeyRelatedField(queryset=Developer.objects.all(), required=True)
-    city = serializers.PrimaryKeyRelatedField(queryset=City.objects.all(), required=True)
+    developer = DeveloperSerializer()  
+    city = CitySerializer()  
 
     class Meta:
         model = PreConstruction
-        fields = "__all__"
+        fields = ['id', 'meta_title', 'meta_description', 'project_name', 'slug', 'storeys', 'total_units', 'price_starts', 'price_end', 'description', 'project_address', 'postal_code', 'latitude', 'longitude', 'occupancy', 'status', 'project_type', 'street_map', 'developer', 'city', 'images']
+        extra_kwargs = {'latitude': {'required':False}, 
+                        'longitude': {'required':False},
+                        'images': {'required':False},
+                        'slug': {'required':False},
+                        'occupancy': {'required':False},
+                        'total_units': {'required':False},
+                        'price_starts': {'required':False},
+                        'price_end': {'required':False},
+                        'description': {'required':False},
+                        'project_address': {'required':False},
+                        'postal_code': {'required':False},
+                        'street_map': {'required':False},
+                        'storeys': {'required':False},
+                        'meta_title': {'required':False},
+                        'meta_description': {'required':False},
+                        'project_type': {'required': True},  # Ensures 'project_type' is required
+                        'status': {'required': True},        
+        }
 
     def create(self, validated_data):
         images_data = validated_data.pop('images', [])
-        developer = validated_data.pop('developer', None)
-        city = validated_data.pop('city', None)
+        developer_data = validated_data.pop('developer')
+        city_data = validated_data.pop('city')
 
-        if not developer or not city:
-            raise serializers.ValidationError("Developer and City are required.")
+        developer = Developer.objects.get_or_create(**developer_data)[0]
+        city = City.objects.get_or_create(**city_data)[0]
 
         preconstruction = PreConstruction.objects.create(developer=developer, city=city, **validated_data)
 
--- preconstruction/api/views.py
+++ preconstruction/api/views.py
@@ -5,25 +5,31 @@ from preconstruction.api.serializers import PreConstructionSerializer, Developer
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 class preconstruction_list(generics.ListCreateAPIView):
+    permission_classes=[];
     queryset = PreConstruction.objects.all()
     serializer_class = PreConstructionSerializer
 
 class precon_details(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [];
     queryset = PreConstruction.objects.all()
     serializer_class = PreConstructionSerializer
 
 class developer_list(generics.ListCreateAPIView):
+    permission_classes = [];
     queryset = Developer.objects.all()
     serializer_class = DeveloperSerializer
 
 class developer_details(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [];
     queryset = Developer.objects.all()
     serializer_class = DeveloperSerializer
 
 class city_list(generics.ListCreateAPIView):
+    permission_classes = [];
     queryset = City.objects.all()
     serializer_class = CitySerializer
 
 class city_details(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [];
     queryset = City.objects.all()
     serializer_class = CitySerializer
