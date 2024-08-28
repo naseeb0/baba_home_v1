1e4268a93a009c1c50d16da344cb393cf6669d3e
naseeb0 <nasheeb.dangi@gmail.com>
2024-08-28 14:40:33 +0545

    Migrations:fixed

--- babahome/settings.py
+++ babahome/settings.py
@@ -134,23 +134,23 @@ WSGI_APPLICATION = 'babahome.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-##
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'homebabacom',
-        'USER': 'naseeb',
-        'PASSWORD': 'DatabaseNaseebPassword',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'homebabacom',
+#         'USER': 'naseeb',
+#         'PASSWORD': 'DatabaseNaseebPassword',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 
