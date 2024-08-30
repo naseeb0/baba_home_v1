a641e7560f52426a1983a2c5df46ec13eb9da42a
naseeb0 <nasheeb.dangi@gmail.com>
2024-08-30 20:21:58 +0545

    CSRF check

--- babahome/settings.py
+++ babahome/settings.py
@@ -103,10 +103,15 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
      "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "https://homebaba.com",
+    'http://homebaba.com'
 ]
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://homebaba.com',
+    'http://homebaba.com/'
+
     # Add other trusted origins here if necessary
 ]
 2. 
